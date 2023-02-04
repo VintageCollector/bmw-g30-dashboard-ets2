c97efe8e7e1d0446e5b4d7d85b9f293349796bd6
Rahul Salin <79052878+VintageCollector@users.noreply.github.com>
2023-02-04 22:08:11 +0300

    Add files via upload

--- src/CAN/rpm.cpp
+++ src/CAN/rpm.cpp
@@ -7,11 +7,25 @@ uint8_t rpm_frame[8] = {0x00, 0x00, 0x00, 0xC0, 0xF0, 0xC4, 0xFF, 0xFF};
 
 
 void canSendRPM(){
-  uint16_t value = s_rpm;
+  uint16_t rpm = s_rpm;
+if(rpm >> 1)
+{
+//rpm_frame[1] = value *0.00659; 
+//rpm_frame[2] = value * 0.007;
 
-rpm_frame[1] = value *0.00659; 
-rpm_frame[2] = value * 0.007;
+rpm_frame[1] = (int(rpm * 1.557) & 0xff); 
+rpm_frame[2] = (int(rpm * 1.557) >> 8);
 
   CAN.sendMsgBuf(CAN_ID, 0, 8, rpm_frame);
- 
+}
+
+else
+
+{
+
+rpm_frame[1] = 0x00; 
+rpm_frame[2] = 0x00;
+
+ CAN.sendMsgBuf(CAN_ID, 0, 8, rpm_frame);
+}
 }
